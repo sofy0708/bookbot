0e084ab082d37f4da530bf8ef047cf2fdd71f228
sofy0708 <linsofia07082000@gmail.com>
2025-08-05 22:28:04 +0200

    update main.py (add get_book_text)

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(book_filepath):
+    with open(book_filepath) as f:
+        book_content = f.read()
+    return book_content
+
+def main():
+    book_content = get_book_text("books/frankenstein.txt")
+    print(book_content)
+    
+main()
